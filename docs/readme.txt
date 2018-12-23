ad9e90b1e2310d334adec95163cbb892b980c903
Mikian Musser <Bobar312@gmail.com>
2018-12-23 10:14:48 -0800

    Added Template files

---
+++ docs/readme.txt
@@ -0,0 +1,38 @@
+[✔️] Done
+[__] Not done
+
+// --
+
+[✔️] Download files
+[__] Open up in atom - Check this box!
+
+// HTML
+[__] Change the title of your website
+[__] Replace one of the pictures on the cards
+[__] Edit the description of one of the cards
+[__] Edit the bottom text of one of the cards
+
+// Type.js
+[__] Change the text in type.js
+[__] Change the speed in type.js
+[__] Change the loop status in type.js
+
+// CSS
+[__] Change the background color
+[__] Change the header color
+[__] Change the card-body color and background
+[__] Change the btn-primary color and background
+
+// Particles.js
+[__] Change the number of particles
+[__] Change the color of the particles
+  // Red: #ff0000
+  // Green: #00ff00
+  // Blue: #0000ff
+[__] Change the shape to 'star' or 'edge'
+[__] Change the 'size'
+[__] Change line_linked to true
+[__] Change the line_linked distance and color
+[__] Change move speed
+[__] Change move direction
+[__] Change out_mode to 'bounce'
---
+++ typed/type.js
@@ -0,0 +1,73 @@
+jQuery(document).ready(function() {
+  var type = new Typed('#type', {
+    strings: [
+      'I',
+      'Love',
+      'Snow!'
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+  });
+  var title1 = new Typed('#cardTitle1', {
+    strings: [
+      'Fortnite!!!!!!!!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title2 = new Typed('#cardTitle2', {
+    strings: [
+      'My p5.js sketch',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title3 = new Typed('#cardTitle3', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title4 = new Typed('#cardTitle4', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title5 = new Typed('#cardTitle5', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title6 = new Typed('#cardTitle6', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+});
